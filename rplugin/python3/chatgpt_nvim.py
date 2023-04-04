e8d90db90efa744a5fe5b6b7d576f99388aada47
Liam <liam@scalzulli.com>
2023-04-03 23:06:44 -0400

    Access first response

--- rplugin/python3/chatgpt_nvim.py
+++ rplugin/python3/chatgpt_nvim.py
@@ -259,7 +259,7 @@ class Model:
       model="gpt-3.5-turbo", messages=[{
         "role": "user", "content": prompt
       }]
-    ).choices[0].text.strip()
+    )['choices'][0]['message']['content'].strip()
 
 @neovim.plugin
 class Plugin:
